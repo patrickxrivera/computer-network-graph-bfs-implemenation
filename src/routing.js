db6781c665425bf2f0df4ef9b3e041b73c13de5d
Patrick Rivera <pxr13@my.fsu.edu>
2018-05-24 18:33:30 -0400

    Implemented find vertex

--- src/routing.js
+++ src/routing.js
@@ -4,7 +4,9 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
 
+const containsValue = value => (acc, curr) => (curr.value === value ? curr : acc);
 /**
  * Edge class
  */
@@ -48,7 +50,7 @@ class Graph {
    * @return null if not found.
    */
   findVertex(value) {
-    // !!! IMPLEMENT ME
+    return this.vertexes.reduce(containsValue(value), null);
   }
 
   /**
@@ -82,78 +84,75 @@ class Graph {
     // Show the route from the start
     this.outputRoute(start);
   }
+
+  addEdge(v1, v2) {
+    v1.edges.push(v2);
+    v2.edges.push(v1);
+  }
 }
 
-/**
- * Helper function to add bidirectional edges
- */
-function addEdge(v0, v1) {
-  v0.edges.push(new Edge(v1));
-  v1.edges.push(new Edge(v0));
-}
-
-/**
- * Main
- */
-
-// Test for valid command line
-const args = process.argv.slice(2);
-
-if (args.length !== 2) {
-  console.error('usage: routing hostA hostB');
-  process.exit(1);
-}
-
-// Build the entire Internet
-// (it's only a model)
-const graph = new Graph();
-const vertA = new Vertex('HostA');
-const vertB = new Vertex('HostB');
-const vertC = new Vertex('HostC');
-const vertD = new Vertex('HostD');
-const vertE = new Vertex('HostE');
-const vertF = new Vertex('HostF');
-const vertG = new Vertex('HostG');
-const vertH = new Vertex('HostH');
-
-addEdge(vertA, vertB);
-addEdge(vertB, vertD);
-addEdge(vertA, vertC);
-addEdge(vertC, vertD);
-addEdge(vertC, vertF);
-addEdge(vertG, vertF);
-addEdge(vertE, vertF);
-addEdge(vertH, vertF);
-addEdge(vertH, vertE);
-
-graph.vertexes.push(vertA);
-graph.vertexes.push(vertB);
-graph.vertexes.push(vertC);
-graph.vertexes.push(vertD);
-graph.vertexes.push(vertE);
-graph.vertexes.push(vertF);
-graph.vertexes.push(vertG);
-graph.vertexes.push(vertH);
-
-// Look up the hosts passed on the command line by name to see if we can
-// find them.
-
-const hostAVert = graph.findVertex(args[0]);
-
-if (hostAVert === null) {
-  console.error(`routing: could not find host: ${args[0]}`);
-  process.exit(2);
-}
-
-const hostBVert = graph.findVertex(args[1]);
-
-if (hostBVert === null) {
-  console.error(`routing: could not find host: ${args[1]}`);
-  process.exit(2);
-}
-
-// Show the route from one host to another
-
-graph.route(hostAVert, hostBVert);
-
-module.exports = Graph;
+// /**
+//  * Main
+//  */
+//
+// // Test for valid command line
+// const args = process.argv.slice(2);
+//
+// if (args.length !== 2) {
+//   console.error('usage: routing hostA hostB');
+//   process.exit(1);
+// }
+//
+// // Build the entire Internet
+// // (it's only a model)
+// const graph = new Graph();
+// const vertA = new Vertex('HostA');
+// const vertB = new Vertex('HostB');
+// const vertC = new Vertex('HostC');
+// const vertD = new Vertex('HostD');
+// const vertE = new Vertex('HostE');
+// const vertF = new Vertex('HostF');
+// const vertG = new Vertex('HostG');
+// const vertH = new Vertex('HostH');
+//
+// graph.addEdge(vertA, vertB);
+// graph.addEdge(vertB, vertD);
+// graph.addEdge(vertA, vertC);
+// graph.addEdge(vertC, vertD);
+// graph.addEdge(vertC, vertF);
+// graph.addEdge(vertG, vertF);
+// graph.addEdge(vertE, vertF);
+// graph.addEdge(vertH, vertF);
+// graph.addEdge(vertH, vertE);
+//
+// graph.vertexes.push(vertA);
+// graph.vertexes.push(vertB);
+// graph.vertexes.push(vertC);
+// graph.vertexes.push(vertD);
+// graph.vertexes.push(vertE);
+// graph.vertexes.push(vertF);
+// graph.vertexes.push(vertG);
+// graph.vertexes.push(vertH);
+//
+// // Look up the hosts passed on the command line by name to see if we can
+// // find them.
+//
+// const hostAVert = graph.findVertex(args[0]);
+//
+// if (hostAVert === null) {
+//   console.error(`routing: could not find host: ${args[0]}`);
+//   process.exit(2);
+// }
+//
+// const hostBVert = graph.findVertex(args[1]);
+//
+// if (hostBVert === null) {
+//   console.error(`routing: could not find host: ${args[1]}`);
+//   process.exit(2);
+// }
+//
+// // Show the route from one host to another
+//
+// graph.route(hostAVert, hostBVert);
+//
+module.exports = { Graph, Vertex };
--- test/routing.test.js
+++ test/routing.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-undef, no-prototype-builtins */
 
-const Graph = require('../src/routing');
+const { Graph, Vertex } = require('../src/routing');
 
 describe('Graph', () => {
   let graph;
@@ -10,7 +10,19 @@ describe('Graph', () => {
     graph = new Graph();
   });
 
-  it('should return true', () => {
-    expect(true).toBe(true);
+  it('should find the vertex that matches the value parameter', () => {
+    const vertA = new Vertex('HostA');
+    const vertB = new Vertex('HostB');
+    graph.addEdge(vertA, vertB);
+    graph.vertexes.push(vertA, vertB);
+    expect(graph.findVertex(vertA.value).value).toBe('HostA');
+  });
+
+  it('should return null when an invalid value is given', () => {
+    const vertA = new Vertex('HostA');
+    const vertB = new Vertex('HostB');
+    graph.addEdge(vertA, vertB);
+    graph.vertexes.push(vertA, vertB);
+    expect(graph.findVertex('Invalid value')).toBe(null);
   });
 });
